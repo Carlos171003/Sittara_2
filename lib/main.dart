f064500fb3bcbab06ab5cd1862f148bf7d07efc3
Carlos171003 <prepa6acy1d.canul.carlos@gmail.com>
2025-11-14 07:59:23 -0600

    refactor: Remove welcome screen and set login as initial route

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'src/features/auth/presentation/screens/login_screen.dart';
-import 'src/features/home/presentation/screens/welcome_screen.dart';
 import 'src/features/home/presentation/screens/menu_screen.dart';
 import 'src/features/home/presentation/screens/home_screen.dart';
+import 'src/features/home/presentation/screens/bistrola57_screen.dart'; // New import
 
 
 void main() => runApp(const SittaraApp());
@@ -15,12 +15,19 @@ class SittaraApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Sittara',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 138, 158, 141),
+          brightness: Brightness.light, // Assuming a light theme
+        ),
+        useMaterial3: true, // Use Material 3 design
+      ),
       initialRoute: '/login',
       routes: {
-        '/welcome': (context) => const WelcomeScreen(),
         '/login': (context) => const LoginScreen(),
         '/home': (context) => const HomeScreen(),
         '/menu': (context) => const MenuScreen(),
+        '/bistrola57': (context) => const Bistrola57Screen(), // New route
       },
     );
   }
